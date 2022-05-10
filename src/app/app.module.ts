e4e0a62a52ee69c3037e91d74da183b634574084
unknown <wangtianjie@xgd.com>
2022-05-10 11:13:58 +0800

    add code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        { path: 'crisis-list', component: CrisisListComponent },
+        { path: 'hero-list', component: HeroListComponent },
+        { path: '', redirectTo: '/hero-list', pathMatch: 'full' },
+        { path: '**', component: PageNotFoundComponent }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
